117a0c39d53b9463749e21f35b5495a9b861adba
abdech <abdech33@gmail.com>
2022-11-22 22:52:55 +0100

    Added the str_fun which prints to the stdout

---
+++ pcent_fun.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *pcen_fun - prints one % 
+ * @args: variadic parameter
+ *
+ * Return: number of characters printed
+ */
+
+int pcent_fun(va_list args)
+{
+	(void)args;
+	
+	_putchar('%');
+	return (1);
+}	
