67267a79a6deb0d9860a9297590ab3f9b1a1185f
abdech <abdech33@gmail.com>
2022-11-22 22:28:11 +0100

    Updated the main.h file
    Added the struct that save the function to be printed and character to be checked

--- main.h
+++ main.h
@@ -8,5 +8,19 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int (*is_special_character(const char *format))(va_list);
+
+/**
+ *struct func - struct that save special characters and their functions
+ *@t: character to be compared
+ *@f: function that print to the standard output
+ *
+ */
+
+typedef struct func
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
 
 #endif
