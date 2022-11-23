97ed05a7518d8952a13aa0b3e3aa015394bc3158
abdech <abdech33@gmail.com>
2022-11-23 03:10:50 +0100

    Add int_fun.c that checks for i as a special character

--- int_fun.c
+++ int_fun.c
@@ -10,7 +10,8 @@
 
 int int_fun(va_list args)
 {
-	int i, count = 0, numb;
+	int i = 0, count = 0;
+	int numb;
 	int arr[10];
 	char x[1];
 
@@ -18,21 +19,22 @@ int int_fun(va_list args)
 	numb = va_arg(args, int);
 
 	/* look for remainder and increment it */
-	for (i = 0; numb != 0; i++)
+	while (numb != 0)
 	{
 		arr[i] = (numb % 10);
 		numb = numb / 10;
-		if (numb != 0)
-			i++;
-		else
+		if (numb == 0)
 			break;
+		else 
+			i++;
 	}
-
+	
 	/* print the array indexes to stdout*/
-	for (i = 0; i >= 0; i++)
+	while (i >= 0)
 	{
 		x[0] = ('0' + arr[i]);
 		count += write(1, x, 1);
+		i--;
 	}
-	return (0);
+	return (count);
 }
